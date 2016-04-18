4f9825f7fbf6858cebd2e4dc1e155b78fab3ad18
Jordomav <jordomav@gmail.com>
2016-04-18 13:59:13 -0700

    updated gitignore

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,64 +1,18 @@
 @extends('layouts.app')
 @section('content')
-{{--<div class="container">--}}
-    {{--<div class="row">--}}
-        {{--<h1 class="main">Jeopardy!</h1>--}}
-    {{--</div>--}}
-    {{--<div class="row">--}}
-        {{--<h4 href="" class="start">PLAY</h4>--}}
-    {{--</div>--}}
-{{--</div>--}}
-    <div class="container">
-        <div class="row">
-            @foreach($categories as $category)
-                <div class="col-lg-2 category">{{$category->title}}</div>
-            @endforeach
-            <div class="row">
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-            </div>
-        </div>
+<div class="container">
+    <div class="row">
+        <h1 class="main">Jeopardy!</h1>
     </div>
-
+    <div class="row">
+        <h4 class="start">PLAY</h4>
+    </div>
+</div>
     {{--<div class="container">--}}
         {{--<div class="row">--}}
-            {{--<h1 class="question">Here is a question that has to do with the value</h1>--}}
+            {{--@foreach($categories as $category)--}}
+                {{--<div class="col-lg-2 category">{{$category->title}}</div>--}}
+            {{--@endforeach--}}
         {{--</div>--}}
     {{--</div>--}}
 @endsection
