34cf0a6022543a05e5e7249c4d80749e26a69cee
Jordomav <jordomav@gmail.com>
2016-04-18 13:03:32 -0700

    styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,51 +8,57 @@
         {{--<h4 href="" class="start">PLAY</h4>--}}
     {{--</div>--}}
 {{--</div>--}}
+    {{--<div class="container">--}}
+        {{--<div class="row">--}}
+            {{--@foreach($categories as $category)--}}
+                {{--<div class="col-lg-2 category">{{$category->title}}</div>--}}
+            {{--@endforeach--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-lg-2 value">$100</div>--}}
+                {{--<div class="col-lg-2 value">$100</div>--}}
+                {{--<div class="col-lg-2 value">$100</div>--}}
+                {{--<div class="col-lg-2 value">$100</div>--}}
+                {{--<div class="col-lg-2 value">$100</div>--}}
+                {{--<div class="col-lg-2 value">$100</div>--}}
+            {{--</div>--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-lg-2 value">$200</div>--}}
+                {{--<div class="col-lg-2 value">$200</div>--}}
+                {{--<div class="col-lg-2 value">$200</div>--}}
+                {{--<div class="col-lg-2 value">$200</div>--}}
+                {{--<div class="col-lg-2 value">$200</div>--}}
+                {{--<div class="col-lg-2 value">$200</div>--}}
+            {{--</div>--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-lg-2 value">$300</div>--}}
+                {{--<div class="col-lg-2 value">$300</div>--}}
+                {{--<div class="col-lg-2 value">$300</div>--}}
+                {{--<div class="col-lg-2 value">$300</div>--}}
+                {{--<div class="col-lg-2 value">$300</div>--}}
+                {{--<div class="col-lg-2 value">$300</div>--}}
+            {{--</div>--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-lg-2 value">$400</div>--}}
+                {{--<div class="col-lg-2 value">$400</div>--}}
+                {{--<div class="col-lg-2 value">$400</div>--}}
+                {{--<div class="col-lg-2 value">$400</div>--}}
+                {{--<div class="col-lg-2 value">$400</div>--}}
+                {{--<div class="col-lg-2 value">$400</div>--}}
+            {{--</div>--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-lg-2 value">$500</div>--}}
+                {{--<div class="col-lg-2 value">$500</div>--}}
+                {{--<div class="col-lg-2 value">$500</div>--}}
+                {{--<div class="col-lg-2 value">$500</div>--}}
+                {{--<div class="col-lg-2 value">$500</div>--}}
+                {{--<div class="col-lg-2 value">$500</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
     <div class="container">
         <div class="row">
-            @foreach($categories as $category)
-                <div class="col-lg-2 category">{{$category->title}}</div>
-            @endforeach
-            <div class="row">
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-                <div class="col-lg-2 value">$100</div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-                <div class="col-lg-2 value">$200</div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-                <div class="col-lg-2 value">$300</div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-                <div class="col-lg-2 value">$400</div>
-            </div>
-            <div class="row">
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-                <div class="col-lg-2 value">$500</div>
-            </div>
+            <h1 class="question">Here is a question that has to do with the value</h1>
         </div>
     </div>
 @endsection
