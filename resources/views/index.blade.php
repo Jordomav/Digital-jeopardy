f22a72e0035ecbb23b8dbc39bcb01c9fbb74fb1c
Jordomav <jordomav@gmail.com>
2016-04-18 14:34:16 -0700

    added gulpfile/npm/bower

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,4 +15,10 @@
             {{--@endforeach--}}
         {{--</div>--}}
     {{--</div>--}}
+
+    {{--<div class="container">--}}
+        {{--<div class="row">--}}
+            {{--<h1 class="question">Here is a question that has to do with the value</h1>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,15 +2,13 @@
 <html>
     <head>
         <title>Jeopardy</title>
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/build/css/css-deps.css">
+        <link rel="stylesheet" href="/build/css/app.css">
         <link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="styles.css">
     </head>
     <body>
         @yield('content')
     </body>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.3/angular.min.js"></script>
-    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="/build/js/deps.js"></script>
+    <script src="/build/js/app.js"></script>
 </html>
