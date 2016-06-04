fb91a21e1081263a869dcb5a775e47c6eb983cf4
supaheckafresh <neilstrain@gmail.com>
2016-06-04 13:32:22 -0700

    Display game name in edit game view

--- resources/views/categories/admin-categories.blade.php
+++ resources/views/categories/admin-categories.blade.php
@@ -3,7 +3,7 @@
     <body style="background-color: white;">
     <div class="container">
         <div class="row">
-            <h1>Create your Jeopardy game.</h1>
+            <h1>{{ $game->name }} Jeopardy</h1>
             <h2>Create 6 categories</h2>
             <div>
                 @foreach($categories as $category)
