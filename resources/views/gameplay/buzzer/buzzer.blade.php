04ec434c66357f43ba7e1411ce20c950877f0730
supaheckafresh <neilstrain@gmail.com>
2016-06-04 20:37:19 -0700

    Center buzzer horizontally and add some top padding

--- resources/views/gameplay/buzzer/buzzer.blade.php
+++ resources/views/gameplay/buzzer/buzzer.blade.php
@@ -4,7 +4,7 @@
 
     @if($currentUser)
 
-    <div class="container buzzer-container" data-ng-app="jeopardyApp" data-ng-controller="buzzerController as buzzer"
+    <div class="container buzzer-container text-center" data-ng-app="jeopardyApp" data-ng-controller="buzzerController as buzzer"
          data-ng-init="buzzer.init('{{ $currentUser }}')">
         <button data-ng-click="buzzer.broadcastToAllPlayersInGame()"
                 data-ng-disabled="buzzer.disableBuzzer()"
@@ -17,6 +17,11 @@
         </div>
 
         <button data-ng-click="buzzer.getFirstPlayerWhoBuzzedIn()">Get First</button>
+
+        <div>
+            {{-- TODO: Display player score --}}
+            <p class="score">$@{{ buzzer.currentUserScore || 0 }}</p>
+        </div>
         
     </div>
 
